38b06a0cedb4954e75817d5be410e2524a997b6e
Ryan Zhu <86477098+rzhu-0812@users.noreply.github.com>
2024-10-18 16:23:54 +0000

    hehehehehehehehehehehehhehehehhehehe

--- Main.java
+++ Main.java
@@ -1,7 +1,16 @@
-public class Main{
+import java.util.*;
+
+public class Main {
     public static void main(String[] args) {
-        WordChecker w = new WordChecker(ArrayList);
-        
+       ArrayList<String> listOne = new ArrayList<String>();
+       listOne.add("an");
+       listOne.add("band");
+       listOne.add("band");
+       listOne.add("abandon");
+    
+       WordChecker y = new WordChecker(listOne);
+
+       System.out.println(y.isWordChain());
     }
 }
 
--- WordChecker.java
+++ WordChecker.java
@@ -1,8 +1,44 @@
-import java.util.ArrayList;
+import java.util.*;
+
 public class WordChecker {
-    public WordChecker(ArrayList<String> list)
-    {
+    /** Initialized in the constructor and contains no null elements */
+    private ArrayList<String> wordList;
+
+    public WordChecker(ArrayList<String> list) {
         wordList = list;
     }
 
+    /**
+     * Returns true if each element of wordList (except the first) contains the
+     * previous
+     * element as a substring and returns false otherwise, as described in part (a)
+     * Precondition: wordList contains at least two elements.
+     * Postcondition: wordList is unchanged.
+     */
+    public boolean isWordChain() {
+        boolean wordChain = true;
+
+        for (int i = 1; i < wordList.size(); i++) {
+            String before = wordList.get(i - 1);
+            String after = wordList.get(i);
+
+            if (after.indexOf(before) == -1) {
+                wordChain = false;
+            }
+        }
+
+        return wordChain;
+    }
+
+    /**
+     * Returns an ArrayList<String> based on strings from wordList that start
+     * with target, as described in part (b). Each element of the returned ArrayList
+     * has had
+     * the initial occurrence of target removed.
+     * Postconditions: wordList is unchanged.
+     * Items appear in the returned list in the same order as they appear in
+     * wordList.
+     */
+    public ArrayList<String> createList(String target) {
+        /* to be implemented in part (b) */ 
 }
